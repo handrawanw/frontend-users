a0f01371b44abc48e1cabf4d1a0b9a962c40a819
handrawanw <42910429+handrawanw@users.noreply.github.com>
2020-03-08 01:04:14 +0700

    add function and validation

--- src/components/ProfileComponents/EditProfile.js
+++ src/components/ProfileComponents/EditProfile.js
@@ -1,86 +1,102 @@
-import React from 'react'
+import React,{useRef} from 'react';
+import Swal from "sweetalert2";
+import EditProfileForm from "./EditProfileForm";
 
-function EditProfile(props) {
+export default function EditProfile() {
+  let info={
+    files:"",
+    name:"",
+    email:"",
+    country:"",
+  },cInfo={
+    files:"",
+    name:"",
+    email:"",
+    country:"",
+  };
+
+  function Save(event){
+    info[event.target.name]=event.target.value;
+    cInfo[event.target.name]=event.target;
+  }
+
+  function Kirim(e) {
+    e.preventDefault();
+    let prosesCek=new Promise((resolve,reject)=>{
+
+        for (let x of Object.keys(info)) {
+            for (let y of Object.values(info)) {
+                if(info.hasOwnProperty(x)){
+                    if(y===""){
+                        reject("input is empty");
+                    }
+                }
+            }
+        }
+           resolve(true);
+    });
+
+    prosesCek.then(res=>{
+
+      if(typeof cInfo['files']==="object"){
+        let tipeFile=["image/png"];
+        for (var i = 0; i < cInfo['files'].files.length; i++) {
+          if(typeof cInfo['files'].files!=="undefined"){
+            if(!tipeFile.includes(cInfo['files'].files[i].type)){
+              throw new Error("Files tipe not support");
+            }else{
+              return res;
+            }
+          }//end files tipe
+        }//end for
+      }//end var object
+
+    }).then(res=>{
+        if(typeof info['name']==="string"){
+          if(/^([A-Z]|[a-z]|[\_])+$/g.test(info['name'])){
+            return res;
+          }else{
+            throw new Error("Invalid name");
+          }
+        }//end var string
+
+    }).then(res=>{
+        if(typeof info['email']==="string"){
+          if(/^([A-Z]|[a-z]|[0-9]+[\@]+[a-z]+[\.]+[a-z])+$/g.test(info['email'])){
+            return res;
+          }else{
+            throw new Error("Invalid email");
+          }
+        }//end var string
+    }).then(res=>{
+        if(typeof info['country']==="string"){
+          if(info['country']==="none"){
+            throw new Error("Country not selected");
+        }else{
+          if(/^([\<|[\"]|[\']|[\`]|[\>])+$/g.test(info['country'])){
+            throw new Error("Invalid country");
+          }else{
+            return res;
+          }
+        }
+
+        }//end var string
+
+    }).then(res=>{
+      alert(res)
+        //axios
+    }).catch(err=>{
+      Swal.fire({
+          icon:'error',
+          title:err,
+          showConfirmButton:false,
+          timer:1500,
+      });
+    });
+
+  }
+
     return (
-        <div className="page-wrapper no-padding">
-            <div className="page-content no-padding mt-0">
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-sm-12">
-                            <div className="page-title-box">
-                                <div className="float-right">
-                                    <ol className="breadcrumb">
-                                    <li className="breadcrumb-item"><a href="javascript:void(0);">Dashboard</a></li>
-                                    <li className="breadcrumb-item"><a href="javascript:void(0);">User</a></li>
-                                    <li className="breadcrumb-item active">Profile</li>
-                                    </ol>{/*end breadcrumb*/}
-                                </div>{/*end /div*/}
-                                <h4 className="page-title">Edit Profile</h4>
-                            </div>{/*end page-title-box*/}
-                        </div>{/*end col*/}
-                    </div>{/*end row*/}
-
-                    <div className="row">
-                        <div className="col-12">
-                            <div className="row">
-                                <div className="col-lg-12" style={{margin: 0, padding: '8px'}}>
-                                    <div className="card">
-                                        <div className="card-body col-lg-8 mx-auto">
-                                            <h3 className="mb-4" align="center">Edit Profile</h3>
-                                            <form method="post" className="card-box">
-                                                <div className="form-group row">
-                                                    <div className="col-md-12">
-                                                        <input type="file" name="profileFile" id="input-file-now-custom-1" className="dropify" data-default-file="../assets/images/no_picture.jpg" />
-                                                    </div>
-                                                </div>
-                                                <div className="form-group row">
-                                                    <div className="col-md-12">
-                                                        <div className="input-group">
-                                                            <div className="input-group-prepend">
-                                                                <span className="input-group-text"><i className="far fa-user" /></span>
-                                                            </div>
-                                                            <input type="text" id="example-input1-group1" name="name" className="form-control" placeholder="Full Name" />
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div className="form-group row">
-                                                    <div className="col-md-12">
-                                                        <div className="input-group">
-                                                            <div className="input-group-prepend">
-                                                                <span className="input-group-text"><i className="far fa-envelope" /></span>
-                                                            </div>
-                                                            <input type="email" id="example-input2-group2" name="email" className="form-control" placeholder="Email" />
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div className="form-group row">
-                                                    <div className="col-md-12">
-                                                        <select className="form-control" name="Country">
-                                                            <option value>Country</option>
-                                                            <option value="Indonesia">Indonesia</option>
-                                                            <option value="Singapore">Singapore</option>
-                                                            <option value="Malaysia">Malaysia</option>
-                                                            <option value="Thailand">Thailand</option>
-                                                        </select>
-                                                    </div>
-                                                </div>
-                                                <div className="form-group row">
-                                                    <div className="col-md-12">
-                                                        <button className="btn btn-primary btn-sm px-4 mt-3 float-right mb-0">Save Changes</button>
-                                                    </div>
-                                                </div>
-                                            </form>
-                                    
-                                        </div>                                            
-                                    </div>
-                                </div> {/*end col*/}                                          
-                            </div>{/*end row*/}                            
-                        </div>{/*end col*/}
-                    </div>{/*end row*/}
-                </div>{/* container */}
-            </div>
-        </div>
+      <EditProfileForm Save={Save} Kirim={Kirim} />
     )
 }
-
-export default EditProfile;
